2ad3b31e4ef70172e9397df1c1c3b921d33ece65
nxthao <nxthao0701@gmail.com>
2017-12-24 01:55:00 +0700

    initial commit

---
+++ web/test.php
@@ -0,0 +1,23 @@
+<?php
+    if (isset($_POST['button']))
+    {
+        system("gpio write 7 1");
+    }
+    else if (isset($_POST['btn']))
+    {
+        system("gpio write 7 0");
+    }
+?>
+<html>
+<body>
+    <form method="post">
+    <p>
+        <button name="button">On</button>
+    </p>
+    <p>
+        <button name="btn">Off</button>
+    </p>
+    </form>
+</body>
+</html>
+
