9295c7208ebc554ffd033b30a8bf733c8f0e1a3c
nxthao <xthaoqt@gmail.com>
2018-01-02 10:33:46 +0700

    eighth commit

--- test/data.py
+++ test/data.py
@@ -20,7 +20,7 @@ ser = serial.Serial(
 
 
 # cai dat thong so infuxdb
-host = "192.168.1.6"
+host = "192.168.1.2"
 port = "8086"
 dbname = "demo"  #tao database
 client = InfluxDBClient(host=host, port=port, database=dbname) # tao object InfluxDB 
@@ -51,28 +51,25 @@ class Producer(threading.Thread):
         Append random data to data list at random time.
         """
        
-        # set time
 
         while True:
-            print(time.clock())
             t = time.clock() 
             try:
                 while True:
-                    #print(time.clock())
                     a = ser.read()
                     if a == '\xff':
                         #wait to receive data from uart
                         #dung time.sleep de cho lay het du lieu 1 lan truyen
                         time.sleep(0.003)
-                        #doc du lieu dua vao b
-                        b = ser.read(ser.inWaiting())
-                        """ in gia tri hexa cua a,
-                        (chi cach nay in duoc gtri hexa cua string)
                         """
+                        doc du lieu dua vao b(b la tat ca du lieu uart gui
+                        ,ngoai tru a)
+                        """
+                        b = ser.read(ser.inWaiting())
+                        print " ".join(hex(ord(n)) for n in b)
+                        
                         #gia tri c chinh la gia tri toan bo data minh nhan dc
                         c = a + b[0] + b[1] + b[2] + b[3] + b[4] + b[5]
-                        print " ".join(hex(ord(n)) for n in a)
-                        print " ".join(hex(ord(n)) for n in b)
                         print " ".join(hex(ord(n)) for n in c)
                         data_t = [] 
 
@@ -86,6 +83,7 @@ class Producer(threading.Thread):
                             if test_crc == 0:
                                 i = 0
                                 while i <= 6:  
+                                    #dua du lieu vao data,nhung dua kieu insert
                                     self.data.insert(0, c[i])
                                     i += 1 
                                 print(" data send ok \n")
@@ -98,9 +96,7 @@ class Producer(threading.Thread):
                     time.sleep(1)
                     if (time.clock() - t) >= 20:
                         ser.write('\x10'+'\x26'+'\xFF'+'\x5B'+'\xE5')
-                        #print("resend data")
 
-  
 
             # de bat khi khong nhan duoc du lieu
             # (thong qua bat loi khong co chi so trong mang)
@@ -112,10 +108,6 @@ class Producer(threading.Thread):
 
 
             self.condition.acquire()
-            print('condition acquired by {}'.format(self.name))
-
-            # print('{} appended to list by {}'.format(self.data, self.name))
-            print " ".join(hex(ord(n)) for n in self.data)
             
             counter = self.counter.pop()
             counter += 1
@@ -123,10 +115,8 @@ class Producer(threading.Thread):
             
             print('tin hieu chua xu ly {}'.format(counter))
 
-            print('condition notified by {}'.format(self.name))
             self.condition.notify()
             
-            print('condition released by {}'.format(self.name))
             self.condition.release()
             time.sleep(1)
 
@@ -145,15 +135,12 @@ class Producer(threading.Thread):
                             if test_crc2 == 0:
                             
                                 self.condition.acquire()
-                                print('condition acquired by {}'.format(self.name))
 
                                 # tao vong lap de nhan gia tri
                                 n = 0
                                 while n <= 6:
                                     self.data.insert(0, b[m + n])
                                     n += 1      
-                                #print('{} appended to list by {}'.format(self.data, self.name))
-                                print " ".join(hex(ord(n)) for n in self.data)
             
                                 counter = self.counter.pop()
                                 counter += 1
@@ -161,10 +148,8 @@ class Producer(threading.Thread):
             
                                 print('tin hieu chua xu ly {}'.format(counter))
 
-                                print('condition notified by {}'.format(self.name))
                                 self.condition.notify()
             
-                                print('condition released by {}'.format(self.name))
                                 self.condition.release()
 
                                 print(" data 2 send ok \n")
@@ -196,7 +181,6 @@ class Consumer1(threading.Thread):
         state_previous = False
         while True:
             self.condition.acquire()
-            print('condition acquired by {}'.format(self.name))
             data_t = []
             while True:
                 if self.data:
@@ -207,7 +191,6 @@ class Consumer1(threading.Thread):
                         data_t.append(t)
                         i += 1
 
-                    #print('{} popped from list by {}'.format(data_t, self.name))
                     print " ".join(hex(ord(n)) for n in data_t)
                     
                     counter = self.counter.pop()
@@ -217,10 +200,8 @@ class Consumer1(threading.Thread):
                     
                     break #thoat khoi vong lap
 
-                print('conditon wait by {}'.format(self.name))
                 self.condition.wait()
 
-            print('condition released by {}'.format(self.name))
             self.condition.release()
 
             #kiem tra led
@@ -256,7 +237,7 @@ class Consumer1(threading.Thread):
                     client.write_points(json_body) # viet data tu json den InfluxDB
 
                     # neu nhiet do qua lon se gui sms ve phone
-                    if ord(data_t[4]) >= 40:
+                    if ord(data_t[4]) >= 300:
                         client_sms.messages.create(
                             to="+841655240171",
                             from_="+13342125125",
@@ -299,7 +280,25 @@ class Consumer1(threading.Thread):
                     
                     client.write_points(json_body) # viet data tu json den InfluxDB
                 
-          
+                elif data_t[2] == '\x27':
+                    print("xu ly data khong khi") 
+                    k1 = float(ord(data_t[4]))
+
+                    # dua data khong khi vao infuxdb
+                    print("gia tri: {}".format(k1))
+                    json_body = [
+                        {
+
+                            "measurement": "khong_khi_1",
+                            "fields": {
+                                "value": k1,
+                            }
+                        }
+                    ]
+                    
+                    client.write_points(json_body) # viet data tu json den InfluxDB
+                    print("xu ly xong tram 1 \n \n ") 
+
             elif data_t[1] == '\x20':
                 # kiem tra xem nhiet do hay do am 
                 if data_t[2] == '\x24':
@@ -359,6 +358,23 @@ class Consumer1(threading.Thread):
                            }
                         }
                     ]
+                    client.write_points(json_body) # viet data tu json den InfluxDB
+                elif data_t[2] == '\x27':
+                    print("xu ly data khong khi") 
+                    k2 = float(ord(data_t[4]))
+
+                    # dua data khong khi vao infuxdb
+                    print("gia tri: {}".format(k2))
+                    json_body = [
+                        {
+
+                            "measurement": "khong_khi_2",
+                            "fields": {
+                                "value": k2,
+                            }
+                        }
+                    ]
+                    
                     client.write_points(json_body) # viet data tu json den InfluxDB
 
 if __name__ == '__main__':
